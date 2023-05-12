4d58355cd1117693f02a65d9a2d7ece3dc4ab9d8
kevbot8k <kenan.farmer@colorado.edu>
2023-05-11 21:25:47 -0400

    added finder.sh to folder
    completed the work for finder.sh with error checking and error codes.
    need to change messages to something more intuitive and will copy the
    top part for writer.sh

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 if [ -z "$1" ]
 then
@@ -5,9 +6,24 @@ then
 	exit 1;
 fi
 
+if [ ! -d "$1" ]; then
+    echo "$1 is not a directory";
+    exit 1;
+fi
 
 if [ -z "$2" ] 
 then
 	echo "second arg not supplied";
 	exit 1;
 fi
+
+FILE_COUNT=$(ls $1 | wc -l);
+LINE_MATCH=0;
+
+for file in $(ls $1)
+do
+    MATCH_FOR_FILE=$(grep $2 "$1/$file" | wc -l)
+    LINE_MATCH=$(( $LINE_MATCH + $MATCH_FOR_FILE ))
+done
+
+echo "The number of files are $FILE_COUNT and the number of matching lines are $LINE_MATCH"
