c8c5e2d67ad7b37e1ec92441756ac1c46ded0798
kenanfarmer <kenanfarmer@gmail.com>
2023-05-14 15:11:50 -0400

    adding opening and error handling

---
+++ finder-app/writer.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <syslog.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+
+int main(int argc, char **argv) {
+    openlog(NULL, 0, LOG_USER);
+
+    if (argc < 2) {
+        syslog(LOG_ERR, "Invalid Number of arguments: %d", argc);
+        exit(1);
+    }
+
+    int fd = open(argv[1], O_CREAT | O_TRUNC | O_WRONLY, S_IWGRP | S_IROTH | S_IWUSR | S_IRUSR);
+    if (fd == -1) {
+        syslog(LOG_ERR, "File failed to open");
+        exit(1);
+    }
+
+    ssize_t bytesWritten = write(fd, argv[2], sizeof argv[2]);
+    if (bytesWritten == -1) {
+        syslog(LOG_ERR, "Failed to write to file");
+        exit(1);
+    }
+
+}
